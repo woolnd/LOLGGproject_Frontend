25b2f0ecc382e21007acb5d035f417442437a80c
woolnd <98391474+woolnd@users.noreply.github.com>
2023-03-09 01:46:44 +0900

    FEAT:descript

---
+++ src/addition/Descript.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Descript.css";
+const Descript = () => {
+  return (
+    <div className="l-footer">
+      <div className="footer">
+        <div className="footer__copyright">
+          © 2012-2023 LOL.GG. LOL.GG isn’t endorsed by Riot Games and doesn’t
+          reflect the views or opinions of Riot Games or anyone officially
+          involved in producing or managing League of Legends. League of Legends
+          and Riot Games are trademarks or registered trademarks of Riot Games,
+          Inc. League of Legends © Riot Games, Inc.
+        </div>
+        <ul className="footer__sns"></ul>
+      </div>
+    </div>
+  );
+};
+
+export default Descript;
